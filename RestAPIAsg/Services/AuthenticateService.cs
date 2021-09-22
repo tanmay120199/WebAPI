1631351db641c48f14e6118ed6057ad6b5afeac5
tanmay120199 <35444397+tanmay120199@users.noreply.github.com>
2021-09-22 00:58:47 -0700

    Add files via upload

---
+++ RestAPIAsg/Services/AuthenticateService.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using RestAPIAsg.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestAPIAsg.Services
+{
+    public class AuthenticateService : IAuthenticateService
+    {
+        private readonly AppSettings _appSettings;
+        public AuthenticateService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+        
+        private List<User> users=new List<User>(){
+            new User{UserId=1,FirstName="Tanmay",LastName="Gupta",
+                    UserName="tanmaygupta",Password="TanmayGupta"
+            },
+            new User{UserId=2,FirstName="Rakshit",LastName="Hr",
+                    UserName="rakshithr",Password="RakshitHr"
+            }
+        };
+        public User Authenticate(string userName, string password)
+        {
+            var user = users.SingleOrDefault(x => x.UserName == userName && x.Password == password);
+
+            //return null if user not found
+            if (user == null)
+            {
+                return null;
+            }
+            //if user found
+            var tokenHandler = new JwtSecurityTokenHandler();  //creating token handler
+            var key = Encoding.ASCII.GetBytes(_appSettings.Key);  //create key to encrypt
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name,user.UserId.ToString()),
+                    new Claim(ClaimTypes.Role,"Admin"),
+                    new Claim(ClaimTypes.Version,"V5.0")
+                }),
+                Expires = DateTime.UtcNow.AddDays(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            user.Password = null;
+
+            return user;
+        }
+
+        public string ValidateToken(string token)
+        {
+            if (token == null)
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Key);
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+
+                // return user id from JWT token if validation successful
+                return userId.ToString();
+            }
+            catch
+            {
+                // return null if validation fails
+                return null;
+            }
+        }
+    }
+}
---
+++ RestAPIAsg/Services/IAuthenticateService.cs
@@ -0,0 +1,14 @@
+﻿using RestAPIAsg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPIAsg.Services
+{
+    public interface IAuthenticateService
+    {
+        User Authenticate(string userName, string password);
+        string ValidateToken(string token);
+    }
+}
